90178ac7577a94da91bc8dea40066579ccc276a3
Rocco <roccosubscription@gmail.com>
2017-06-18 20:40:14 -0400

    added a few more menu choices

--- Formulae.cbl
+++ Formulae.cbl
@@ -10,12 +10,16 @@
        FILE SECTION.
        WORKING-STORAGE SECTION.
 
+       01  MENU-CHOICE             PIC 9           VALUE 0.
+
        01  RADIUS                  PIC 9(9)V99     VALUE 1.
        01  DIAMETER                PIC 9(9)V99     VALUE 2.
        01  PI                      PIC 9V999999    VALUE 3.141592.
        01  V-LENGTH                PIC 9(9)V99     VALUE 1.
        01  WIDTH                   PIC 9(9)V99     VALUE 1.
        01  HEIGTH                  PIC 9(9)V99     VALUE 1.
+       01  BASE                    PIC 9(9)V99     VALUE 1.
+
 
        01  CIRCUMFRENCE            PIC 9(15)V99    VALUE 6.28.
        01  VOLUME                  PIC 9(18)V99    VALUE 1.
@@ -27,6 +31,14 @@
             DISPLAY "Hello.  Please chose your option:"
             DISPLAY "1.  Circle".
             DISPLAY "2.  Sphere".
-            DISPLAY "3.  Square".
-            STOP RUN.
-       END PROGRAM YOUR-PROGRAM-NAME.
+            DISPLAY "3.  Triangle".
+            DISPLAY "4.  Square".
+            DISPLAY "5.  Rectangular".
+            DISPLAY "6.  Trapezoid".
+            DISPLAY "7.  Pentagon".
+            DISPLAY "8.  Hexagon".
+            DISPLAY "9.  NEXT PAGE".
+            ACCEPT MENU-CHOICE.
+
+       PROGRAM-DONE.
+           STOP RUN.
